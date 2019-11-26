f19ace3452e680fb13d900d4d3ab6d777d3d3030
BJLin <crlc112358@gmail.com>
2019-11-26 14:07:42 +0800

    Initial Commit

---
+++ Abacus_1126/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Abacus_1126
+//
+//  Created by Christian on 2019/11/26.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
